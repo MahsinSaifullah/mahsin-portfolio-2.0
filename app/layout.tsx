2da6d341dd800ff7985d64dad5c8e06cfbd62bd4
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-07-25 17:06:18 +0100

    feat: add page animation using framer motion

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { JetBrains_Mono } from 'next/font/google';
 import './globals.css';
 
 import Header from '@/components/Header';
+import PageTransition from '@/components/PageTransition';
+import StairTransition from '@/components/StairTransition';
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ['latin'],
@@ -24,7 +26,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header />
-        {children}
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
