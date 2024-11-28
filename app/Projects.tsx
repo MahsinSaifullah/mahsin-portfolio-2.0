b580a690f52cc72b294380a8ee3d9ba84d9d6217
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-11-28 10:21:40 +0000

    chore: add color picker project to portfolio

--- app/Projects.tsx
+++ app/Projects.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import React from 'react';
 import SectionHeader from '@/components/SectionHeader';
 import askMePDF from './images/askMePDF.png';
+import colorPicker from './images/colorPicker.png';
 import ProjectItem from './ProjectItem';
 import Tabs from '@/components/Tabs';
 
@@ -25,43 +27,38 @@ const PROJECT_HASH_MAP = {
     githubLink: 'https://github.com/MahsinSaifullah/ask-me-pdf',
     demoLink: 'https://ask-me-pdf.vercel.app/',
   },
+  'Color Picker': {
+    name: 'Color Picker',
+    image: colorPicker,
+    description:
+      'The Color Picker is a React-based web application built with Material-UI, designed for developers and designers. It enables users to create custom color palettes, offering an intuitive interface for selecting and organizing colors. Users can effortlessly copy individual colors to their clipboard and integrate them directly into their projects, streamlining the design workflow',
+    techStack: ['React', 'Javascript', 'Material UI', 'Chroma Js'],
+    githubLink: 'https://github.com/MahsinSaifullah/React-Color-Picker-App',
+    demoLink: 'https://mahsin-react-color-picker.netlify.app/',
+  },
 };
 
-const PROJECTS = [
-  {
-    name: 'Ask Me PDF',
-    image: askMePDF,
-    description:
-      'Ask Me PDF is an AI-powered SaaS web application built with Next.js and Tailwind CSS, designed for seamless interaction with documents. Users can upload PDFs and engage in AI-driven conversations with their content, leveraging OpenAI, LangChain, and Pinecone vectors for efficient information retrieval.',
-    techStack: [
-      'React',
-      'Typescript',
-      'Next JS',
-      'Tailwind css',
-      'Firebase',
-      'Langchain',
-      'OpenAI',
-      'Pinecone',
-      'Clerk Auth',
-    ],
-    githubLink: 'https://github.com/MahsinSaifullah/ask-me-pdf',
-    demoLink: 'https://ask-me-pdf.vercel.app/',
-  },
-];
-
-const PROJECT_NAMES = Object.keys(PROJECT_HASH_MAP);
+const PROJECTS = Object.keys(PROJECT_HASH_MAP);
 
 const Projects = () => {
+  const [activeTab, setActiveTab] = React.useState(0);
+
   return (
     <section className="h-screen flex justify-center flex-col ">
       <SectionHeader index="03" heading="Some Things I've Built" />
       <Tabs
-        tabs={PROJECT_NAMES}
+        tabs={PROJECTS}
         orientation="horizontal"
-        activeIndex={0}
-        onTabSelect={(index) => {}}
+        activeIndex={activeTab}
+        onTabSelect={(index) => {
+          setActiveTab(index);
+        }}
       >
-        <ProjectItem {...PROJECTS[0]} />
+        <ProjectItem
+          {...PROJECT_HASH_MAP[
+            PROJECTS[activeTab] as keyof typeof PROJECT_HASH_MAP
+          ]}
+        />
       </Tabs>
     </section>
   );
