41b8abdeb4c924c922247b845d1e74c7918848ab
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-12-01 19:23:53 +0000

    feat: add onClick on the Link

--- components/NavDrawer.tsx
+++ components/NavDrawer.tsx
@@ -1,41 +1,49 @@
 'use client';
 
-import { Menu } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 import React from 'react';
 import Drawer from '@mui/material/Drawer';
 import Link from 'next/link';
 
 const NavDrawer = () => {
   const [open, setOpen] = React.useState(false);
+
+  const openDrawer = () => setOpen(true);
+  const closeDrawer = () => setOpen(false);
+
   return (
     <>
       <Menu
-        onClick={() => setOpen(true)}
+        onClick={openDrawer}
         className="w-6 h-6 text-lime md:hidden inline-block cursor-pointer"
       />
-      <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
+      <Drawer anchor="right" open={open} onClose={closeDrawer}>
         <ul className="flex space-y-10 flex-col  bg-lightNavy h-screen px-16 text-slate-400 pt-36 text-md">
-          <Link href="#about">
+          <Link href="#about" onClick={closeDrawer}>
             <li className="cursor-pointer hover:text-lime flex flex-col items-center justify-center">
               <span className="text-lime">01.</span> About
             </li>
           </Link>
-          <Link href="#experience">
+          <Link href="#experience" onClick={closeDrawer}>
             <li className="cursor-pointer hover:text-lime flex flex-col items-center justify-center">
               <span className="text-lime">02.</span> Experience
             </li>
           </Link>
-          <Link href="#project">
+          <Link href="#project" onClick={closeDrawer}>
             <li className="cursor-pointer hover:text-lime flex flex-col items-center justify-center">
               <span className="text-lime">03.</span> Projects
             </li>
           </Link>
-          <Link href="#contact">
+          <Link href="#contact" onClick={closeDrawer}>
             <li className="cursor-pointer hover:text-lime flex flex-col items-center justify-center">
               <span className="text-lime">04.</span> Contact
             </li>
           </Link>
         </ul>
+        <X
+          className="h-6 w-6 text-lime absolute right-4 top-4 cursor-pointer"
+          onClick={closeDrawer}
+        />
       </Drawer>
     </>
   );
